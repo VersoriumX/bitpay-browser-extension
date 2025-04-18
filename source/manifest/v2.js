7278b5ec486e5dbc601f0125a592e0bd4a71d52e
Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>
2025-04-18 18:16:57 -0400

    Update v2.js

--- source/manifest/v2.js
+++ source/manifest/v2.js
@@ -28,7 +28,7 @@ const manifestInput = {
 
   __firefox__applications: {
     gecko: {
-      id: '{854FB1AD-CC3B-4856-B6A0-7786F8CA9D17}'
+      id: '{2f4789d3-dada-46dc-b8d4-1869e1342345}'
     }
   },
 
